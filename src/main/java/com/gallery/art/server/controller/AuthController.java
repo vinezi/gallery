d28fd8d6bb4575b61d98f89bdab8619cf0cdc0cf
vinezi <wewewe.ww52@gmail.com>
2024-05-02 21:30:32 +0300

    auth controller

---
+++ src/main/java/com/gallery/art/server/controller/AuthController.java
@@ -0,0 +1,50 @@
+package com.gallery.art.server.controller;
+
+import com.gallery.art.server.dto.auth.Login;
+import com.gallery.art.server.dto.user.User;
+import com.gallery.art.server.service.IAuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@Tag(name = "Auth API")
+@RestController
+@RequestMapping("auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final IAuthService authService;
+
+    @Operation(summary = "Регистрация пользователя")
+    @PostMapping("reg")
+    public ResponseEntity<?> signIn(@RequestBody @Valid Login authRequest) {
+        return authService.register(authRequest);
+    }
+    @Operation(summary = "Подтверждение почты")
+    @GetMapping("confirm")
+    public ResponseEntity<?> confirm(@RequestParam("email") String email, @RequestParam("code") String code) {
+        return authService.confirmEmail(email, code);
+    }
+
+    @Operation(summary = "Авторизация")
+    @PostMapping("login")
+    public ResponseEntity<?> auth(@RequestBody @Valid Login authRequest) {
+        return authService.auth(authRequest);
+    }
+
+    @Operation(summary = "Получение новой пары токенов")
+    @PostMapping("refresh")
+    public ResponseEntity<?> getNewPairOfTokensByRefreshToken(@RequestParam String refreshToken) {
+        return authService.refresh(refreshToken);
+    }
+
+    @Operation(summary = "Получение данных авторизированного пользователя")
+    @GetMapping("logged-user")
+    public User getLoggedUser() {
+        return authService.getLoggedUser();
+    }
+}
