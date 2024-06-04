8e2fb698dc287dbd5f28bce7b44b9c5e05d1121a
vinezi <wewewe.ww52@gmail.com>
2024-06-04 22:06:15 +0300

    add post and tag

--- src/main/java/com/gallery/art/server/service/impl/PostCollectionServiceImpl.java
+++ src/main/java/com/gallery/art/server/service/impl/PostCollectionServiceImpl.java
@@ -1,6 +1,6 @@
 package com.gallery.art.server.service.impl;
 
-import com.gallery.art.server.db.entity.PostCollection;
+import com.gallery.art.server.repository.PostCollectionRepository;
 import com.gallery.art.server.service.IPostCollectionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,5 +12,5 @@ import javax.transaction.Transactional;
 @RequiredArgsConstructor
 public class PostCollectionServiceImpl implements IPostCollectionService {
 
-    private final PostCollection postCollection;
+    private final PostCollectionRepository postCollectionRepository;
 }
