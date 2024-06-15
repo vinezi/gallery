6edb2c13e288ea06674eefbb491a81ad1467fdc5
vinezi <wewewe.ww52@gmail.com>
2024-06-15 23:31:50 +0300

    some fix

--- src/main/java/com/gallery/art/server/controller/PostCollectionController.java
+++ src/main/java/com/gallery/art/server/controller/PostCollectionController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.constraints.Positive;
 
-@Tag(name = "PostCollection API")
+@Tag(name = "Post Collection API")
 @RestController
 @RequestMapping("post-collection")
 @RequiredArgsConstructor
